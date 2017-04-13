8d7062a3cfa01ddeeade3e1d90d77c93d9160ef1
Taylor Everding <taylor1791@users.noreply.github.com>
2017-04-13 10:11:11 -0600

    Do not use vimrc when starting neovim the first time

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ function main() {
   clean_links "$full_file"
   link "$DIR/xdg/nvim" "$full_file"
   if [[ "$result" == "true" ]]; then
-    HOME=/dev/null nvim -c 'PlugUpgrade' -c 'PlugInstall' -c 'qa'
+    nvim -u /dev/null -c 'PlugUpgrade' -c 'PlugInstall' -c 'qa'
   fi
 
   # FIXME Install software: direnv
