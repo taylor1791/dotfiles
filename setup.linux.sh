169d7d59732529ba426beb43a44137eb60265f0f
Taylor Everding <taylor1791@users.noreply.github.com>
2017-04-12 22:03:30 -0600

    Use sudo on apt-stuff

--- setup.linux.sh
+++ setup.linux.sh
@@ -5,16 +5,16 @@ IFS=$'\n\t'
 install() {
   if ! type "$1" > /dev/null; then
     echo "Installing $2..."
-    apt-get install -y "$2"
+    sudo apt-get install -y "$2"
   fi
 }
 
 setup_neovim() {
-  apt-get update
-  install software-properties-common
-  install python-software-properties
-  add-apt-repository -y ppa:neovim-ppa/stable
-  apt-get update
+  sudo apt-get update
+  sudo install software-properties-common
+  sudo install python-software-properties
+  sudo add-apt-repository -y ppa:neovim-ppa/stable
+  sudo apt-get update
 }
 
 setup_neovim
