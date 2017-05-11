8d72891daad29c4c9d8f6e20c666d1435d065d79
Taylor Everding <taylor1791@users.noreply.github.com>
2017-05-11 00:47:17 -0600

    Use a test-run script

---
+++ bin/run-test.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+  bash -x bin/test.sh;
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  DIR="build/taylor1791/dotfiles"
+  docker run \
+    -v `pwd`/$DIR:/root/$DIR \
+    -w /root/$DIR \
+    -e "TRAVIS_OS_NAME=$TRAVIS_OS_NAME" \
+    ubuntu:16.04 \
+    bash -x /root/$DIR/bin/test.sh
+fi
+
