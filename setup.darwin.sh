c90682cb7517641b4b2d424d50a60abf4561081c
Taylor Everding <taylor1791@users.noreply.github.com>
2019-02-15 14:14:07 -0700

    Add gui things to darwin

--- setup.darwin.sh
+++ setup.darwin.sh
@@ -27,3 +27,30 @@ install() {
   fi
 
 }
+
+install_cask() {
+  NAME="$1"
+  SIGNATURE="$2"
+
+  if [[ ! -e "$SIGNATURE" ]]; then
+    echo "Installing $NAME..."
+    brew cask install "$NAME"
+  fi
+}
+
+install_chrome_beta() {
+  SIGNATURE="/Applications/Google Chrome Beta.app"
+  if [[ ! -d "$SIGNATURE" ]]; then
+    install_cask homebrew/cask-versions/google-chrome-beta "$SIGNATURE"
+    mv "/Applications/Google Chrome.app" "/Applications/Google Chrome Beta.app"
+  fi
+}
+
+install_cask amethyst "/Applications/Amethyst.app"
+install_cask keka "/Applications/Keka.app"
+install_cask steam "/Applications/Steam.app"
+install_cask iterm2 "/Applications/iTerm.app"
+install_cask virtualbox "/Applications/VirtualBox.app"
+install_cask vagrant "/opt/vagrant/bin/vagrant"
+install_cask docker "/Applications/Docker.app"
+install_chrome_beta
