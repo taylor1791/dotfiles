7f01062df424fb13ec9e24f8837dae8664d6bde3
Taylor Everding <taylor1791@users.noreply.github.com>
2020-08-21 15:10:07 -0600

    Move configuration linking into scripts.d

--- setup-legacy.sh
+++ setup-legacy.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 function main() {
   PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
 
@@ -17,21 +15,6 @@ function main() {
   # Install really essentail things
   install_app curl
   install_app git
-
-  # Install home config
-  pushd "$DIR/home" > /dev/null
-  for file in `find . -type f -maxdepth 1`; do
-    full_file="$HOME/.$(basename $file)"
-    clean_links "$full_file" "$file"
-    link "$DIR/home/$file" "$full_file"
-  done
-
-  # Install directory configs
-  for file in `find . -type f -mindepth 2 | cut -b 3-`; do
-    mkdir -p $HOME/.$(dirname $file)
-    link "$DIR/home/$file" "$HOME/.$file"
-  done
-  popd > /dev/null
 
   # Install Tools
   install_app shuf coreutils
@@ -84,30 +67,6 @@ function die() {
   echo "$@"
   tput sgr0
   exit 1;
-}
-
-function clean_links() {
-  # Kill pre-existing invalid links
-  if [[ -L "$1" && ! -e "$1" ]]; then
-    echo "Killing $2 invalid link..."
-    rm "$1"
-  fi
-
-  # Backup existing configs
-  if [[ -e "$1" && ! -h "$1" ]]; then
-    echo "Backing up $2..."
-    mv "$1" "$1.$(date +%s)"
-  fi
-}
-
-function link() {
-  if [ ! -e "$2" ]; then
-    echo "Linking $2...";
-    ln -s "$1" "$2"
-    result="true"
-  else
-    result="false"
-  fi
 }
 
 main
