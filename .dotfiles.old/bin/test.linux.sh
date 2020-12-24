c575dde4ba0f2e1937fd2bb4fb9e548bf918a947
Taylor Everding <taylor1791@users.noreply.github.com>
2020-12-23 21:35:27 -0700

    Remove old github action

--- .dotfiles.old/bin/test.linux.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
-apt-get update
-apt-get install -y sudo man-db
-
--- .dotfiles.old/bin/test.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
-mkdir ~/.dotfiles
-cp -R ./* ~/.dotfiles/
-bash -xe ~/.dotfiles/setup.sh
-bash -xe ~/.bash_profile
