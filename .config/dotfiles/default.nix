a9ef4b3a493e93834f309f3a2570714cc718844a
Taylor Everding <taylor1791@users.noreply.github.com>
2021-01-17 11:55:35 -0700

    Install some language linters

--- .config/dotfiles/default.nix
+++ .config/dotfiles/default.nix
@@ -1,11 +1,24 @@
 with (import <nixpkgs> {});
 
+let
+  eslint = nodePackages.eslint;
+  prettier = nodePackages.prettier;
+  write-good = nodePackages.write-good;
+in
 {
   # Environment Management
   inherit direnv lorri;
 
   # Development Tools
   inherit gnupg jq ripgrep sc-im skim tree vim watch watchexec wget zoxide;
+
+  # Language Tools
+  inherit hadolint;             # Docker
+  inherit proselint write-good; # English
+  inherit eslint prettier;      # JavaScript
+  inherit shellcheck;           # Shell
+  inherit tflint;               # Terraform
+  inherit vim-vint;             # Vim
 
   # Investigation Tools
   inherit htop tcptraceroute;
