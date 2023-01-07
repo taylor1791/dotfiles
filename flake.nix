fc9f6c714ffd7e692d88cffc2ed4cc239fee3a3e
Taylor Everding <taylor1791@users.noreply.github.com>
2023-01-07 15:14:51 -0700

    Create a development environment

--- flake.nix
+++ flake.nix
@@ -6,6 +6,18 @@
   };
 
   outputs = inputs: {
+    devShell = inputs.nixpkgs.lib.genAttrs inputs.nixpkgs.lib.systems.flakeExposed (system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      pkgs.mkShell {
+        buildInputs = [
+          pkgs.just
+          pkgs.nix-diff
+        ];
+      }
+    );
+
     nixosConfigurations = {
       korolev = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
