965df1a40fb27470d79538b27f86c320be414250
Taylor Everding <taylor1791@users.noreply.github.com>
2023-10-10 22:26:56 -0600

    Simplify flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,22 @@
 {
-  description = "NixOS modules for Taylor1791's workstations.";
+  description = "NixOS derivations supporting Taylor1791.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
 
-  outputs = inputs: {
-    devShell = inputs.nixpkgs.lib.genAttrs inputs.nixpkgs.lib.systems.flakeExposed (system:
-      let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-      in
-      pkgs.mkShell {
-        buildInputs = [
-          pkgs.just
-          pkgs.nix-diff
-        ];
+  outputs = { nixpkgs, self }: let
+    lib = nixpkgs.lib;
+  in {
+    devShell = lib.genAttrs lib.systems.flakeExposed (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [ just nix-diff ];
       }
     );
 
     nixosConfigurations = {
-      korolev = inputs.nixpkgs.lib.nixosSystem {
+      korolev = lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./hosts/korolev ];
       };
