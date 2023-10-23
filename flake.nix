ea2acececae6cd46b76ab24b3f0cad95e99dd892
Taylor Everding <taylor1791@users.noreply.github.com>
2023-10-22 21:10:23 -0600

    Update to nixpkgs 23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS derivations supporting Taylor1791.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
 
   outputs = { nixpkgs, self }: let
