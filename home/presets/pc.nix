1951c825e0b710dadad3489c273b4af36bd187a4
Taylor Everding <taylor1791@users.noreply.github.com>
2023-11-28 12:52:19 -0700

    Define BORG_REPO as part of the pc preset

--- home/presets/pc.nix
+++ home/presets/pc.nix
@@ -6,9 +6,14 @@ in {
   options.presets.${presetName} = presetOptions.pc;
 
   config = lib.mkIf cfg.enable {
+    home.sessionVariables = {
+      BORG_REPO = "taylor1791@prd-nas-000.idagalaxy.com:/mnt/fire/home/taylor1791/borgbackup/\${HOSTNAME}";
+    };
+
     home.packages = with pkgs; [
       minisign
       rage
+      taylor1791.backup
       taylor1791.borgbackup
       taylor1791.bopen
       taylor1791.mirror
