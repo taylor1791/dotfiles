fd7174245126d35b0db0108b5bf75f532bd416e9
Taylor Everding <taylor1791@users.noreply.github.com>
2024-09-02 16:39:45 -0600

    Use ln for lsp rename

--- nixos/home/modules/neovim/default.nix
+++ nixos/home/modules/neovim/default.nix
@@ -190,7 +190,7 @@ in {
                   vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = bufnr })
                   vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { buffer = bufnr})
                   vim.keymap.set("n", "<leader>dN", vim.diagnostic.goto_prev, { buffer = bufnr})
-                  vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = bufnr })
+                  vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { buffer = bufnr })
                   vim.keymap.set({"n", "v"}, "<leader>la", vim.lsp.buf.code_action, { buffer = bufnr })
                 end,
               })
@@ -205,7 +205,7 @@ in {
                   vim.keymap.del("n", "gT", { buffer = bufnr })
                   vim.keymap.del("n", "<leader>dn", { buffer = bufnr })
                   vim.keymap.del("n", "<leader>dN", { buffer = bufnr })
-                  vim.keymap.del("n", "<leader>lr", { buffer = bufnr })
+                  vim.keymap.del("n", "<leader>nr", { buffer = bufnr })
                   vim.keymap.del({"n", "v"}, "<leader>ca", { buffer = bufnr })
                 end,
               })
