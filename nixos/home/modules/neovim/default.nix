06e9d88650a69185a9953c0acf007096204561e1
Taylor Everding <taylor1791@users.noreply.github.com>
2025-12-13 15:37:01 -0700

    Disable preselection by nvim-cmp and language servers

--- nixos/home/modules/neovim/default.nix
+++ nixos/home/modules/neovim/default.nix
@@ -102,6 +102,8 @@ in {
               local cmp = require('cmp')
 
               cmp.setup({
+                preselect = cmp.PreselectMode.None,
+
                 mapping = cmp.mapping.preset.insert({
                   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                   ['<C-f>'] = cmp.mapping.scroll_docs(4),
