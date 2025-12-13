a4ebedc3fed61f613089ffea89929510f0b80ec3
Taylor Everding <taylor1791@users.noreply.github.com>
2025-12-13 15:59:26 -0700

    Update to NixOS 25.11

--- flake.nix
+++ flake.nix
@@ -8,11 +8,11 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     # The darwin borgBackups have been broken for some time. This is the last know
     # working revision.
